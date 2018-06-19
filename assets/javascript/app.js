0763d13867f8f8b56e60272b1ebe4fbb2c3de39d
Shoaib Naeem <you@example.com>
2018-06-19 12:47:26 -0700

    working on js

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+var timeLeft= 0;
+var correctAnswers: 0;
+var incorrectAnswers: 0;
+var unansweredAnswers: 0;
+
+var answerArray1= ["Italy", "Brazil", "Germany", "France", "Argentina"]; // Who won the 1994 World Cup?
+var answerArray2= ["Real Madrid", "FC Barcelona", "Ateltico Madrid", "Valencia CF", "Athletic Bilbao"] //Estadio Santiago Bernabeu is the home stadium to which La Liga club?//
+var answerArray3= ["Cristiano Ronaldo", "Lionel Messi", "Zinedine Zidane", "Ronaldihno", "Zlatan Ibrahimovic"] // Who holds the record for most Uefa Champions League Goals?//
+var answerArray4= ["11", "8", "12", "10","9"] //According to the official rules, how many players are allowed to play on the field for each team?//
+var answerArray5= ["4", "2", "3", "5", "6"] //The FIFA World Cup takes place every____ years?//
+var answerArray6= ["FIFA", "FOFA", "FFA", "NFA", "MLS"] // The official governing body for soccer is?
+
+
+
